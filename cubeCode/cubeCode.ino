97f7408fd1930fcf3e80b645cabc44af63480786
David McGinnis <dmcginnis427@gmail.com>
2022-04-03 19:59:51 +0200

    switch to bllinky-lite 4

--- cubeCode/cubeCode.ino
+++ cubeCode/cubeCode.ino
@@ -1,61 +1,135 @@
-#define BAUD_RATE 115200
+#include "BlinkyBus.h"
 #include <OneWire.h>
 #include "dht11.h"
+#define BAUD_RATE  19200
+#define commLEDPin    2
+#define measurementInterval   2000
 
-const int dht11Pin =  3;
-const int oneWirePin1 = 10;
-const int oneWirePowerPin1 = 11;
-const int oneWirePin2 = 4;
-const int oneWirePowerPin2 = 5;
+#define BLINKYBUSBUFSIZE  6
+union BlinkyBusUnion
+{
+  struct
+  {
+    int16_t state;
+    int16_t i16_temperature1;
+    int16_t i16_temperature2;
+    int16_t i16_temperature3;
+    int16_t i16_humidity;
+    int16_t i16_moisture;
+  };
+  int16_t buffer[BLINKYBUSBUFSIZE];
+} bb;
+BlinkyBus blinkyBus(bb.buffer, BLINKYBUSBUFSIZE, Serial1, commLEDPin);
+
+struct DS18B20
+{
+  int signalPin;
+  int powerPin;
+  byte chipType;
+  byte address[8];
+  OneWire oneWire;
+  float temp = 0.0;
+};
+
+DS18B20 dS18B20_A;
+DS18B20 dS18B20_B;
+
 const int moisturePin = A0;
-byte oneWireChipType1;
-byte oneWireAddress1[8];
-byte oneWireChipType2;
-byte oneWireAddress2[8];
-OneWire  oneWire1(oneWirePin1);
-OneWire  oneWire2(oneWirePin2);
+const int dht11Pin =  3;
 dht11 DHT11;
 
-struct TransmitData
-{
-  float temperature1;
-  float temperature2;
-  float temperature3;
-  float humidity;
-  float moisture;
-};
-struct ReceiveData
-{
-  int loopDelay = 2000;
-};
+unsigned long now;
+unsigned long lastMeasurement = 0;
 
-void setupPins()
+void setup() 
 {
-  pinMode(A0, INPUT);
+  dS18B20_A.signalPin = 10;
+  dS18B20_A.powerPin = 11;
+  pinMode(dS18B20_A.powerPin, OUTPUT);
+  digitalWrite(dS18B20_A.powerPin, HIGH);    
+  dS18B20_A.oneWire = OneWire(dS18B20_A.signalPin);
+  dS18B20_A.chipType = initDS18B20(dS18B20_A.address, &dS18B20_A.oneWire);
 
-  oneWireChipType1 = initOneWireTemperatureProbe(oneWirePowerPin1, oneWireAddress1, &oneWire1);
-  oneWireChipType2 = initOneWireTemperatureProbe(oneWirePowerPin2, oneWireAddress2, &oneWire2);
+  dS18B20_B.signalPin = 4;
+  dS18B20_B.powerPin = 5;
+  pinMode(dS18B20_B.powerPin, OUTPUT);
+  digitalWrite(dS18B20_B.powerPin, HIGH);    
+  dS18B20_B.oneWire = OneWire(dS18B20_B.signalPin);
+  dS18B20_B.chipType = initDS18B20(dS18B20_B.address, &dS18B20_B.oneWire);
+
+  pinMode(moisturePin, INPUT);
+
+  Serial1.begin(BAUD_RATE);
+  bb.state = 1; //init
+  blinkyBus.start();
+  lastMeasurement = millis(); 
+  
+//  Serial.begin(9600);
+
 }
-void processNewSetting(TransmitData* tData, ReceiveData* rData, ReceiveData* newData)
+
+void loop() 
 {
-  rData->loopDelay = newData->loopDelay;
+  now = millis();
+  if ((now - lastMeasurement) > measurementInterval)
+  {
+    lastMeasurement = now;
+    dS18B20_A.temp = getDS18B20Temperature(&dS18B20_A.oneWire, dS18B20_A.address, dS18B20_A.chipType);
+    dS18B20_B.temp = getDS18B20Temperature(&dS18B20_B.oneWire, dS18B20_B.address, dS18B20_B.chipType);
 
+    bb.i16_temperature2  = (int16_t) (dS18B20_A.temp * 10.0);
+    bb.i16_temperature3  = (int16_t) (dS18B20_B.temp * 10.0);
+
+    bb.i16_moisture = (int16_t) analogRead(moisturePin);
+    
+    DHT11.read(dht11Pin);
+    bb.i16_temperature1 =  (int16_t) DHT11.temperature;
+    bb.i16_humidity = (int16_t) DHT11.humidity;
+
+/*
+    Serial.print(bb.i16_temperature1);
+    Serial.print(" ");
+    Serial.print(bb.i16_temperature2);
+    Serial.print(" ");
+    Serial.print(bb.i16_temperature3);
+    Serial.print(" ");
+    Serial.print(bb.i16_humidity);
+    Serial.print(" ");
+    Serial.print(bb.i16_moisture);
+    Serial.println(" ");
+*/   
+  }
+  blinkyBus.poll();
 }
-boolean processData(TransmitData* tData, ReceiveData* rData)
+
+byte initDS18B20(byte* addr, OneWire* ow)
 {
-  tData->temperature2 = getOneWireTemperature(&oneWire1, oneWireAddress1, oneWireChipType1);
-  tData->temperature3 = getOneWireTemperature(&oneWire2, oneWireAddress2, oneWireChipType2);
-  DHT11.read(dht11Pin);
-  tData->temperature1 = (float)DHT11.temperature;
-  tData->humidity = (float)DHT11.humidity;
-
-  tData->moisture = (float) analogRead(moisturePin);
-  tData->moisture = 100.0 * (1024 - tData->moisture) / 1024.0;
-
-  delay(rData->loopDelay);
-  return true;
+  byte type_s = 0;
+  if ( !ow->search(addr)) 
+  {
+    ow->reset_search();
+    delay(250);
+    return 0;
+  }
+   
+  // the first ROM byte indicates which chip
+  switch (addr[0]) 
+  {
+    case 0x10:
+      type_s = 1;
+      break;
+    case 0x28:
+      type_s = 0;
+      break;
+    case 0x22:
+      type_s = 0;
+      break;
+    default:
+      return 0;
+  } 
+  return type_s;
 }
-float getOneWireTemperature(OneWire* ow, byte* addr, byte chipType)
+float getDS18B20Temperature(OneWire* ow, byte* addr, byte chipType)
 {
   byte i;
   byte data[12];
@@ -86,103 +160,5 @@ float getOneWireTemperature(OneWire* ow, byte* addr, byte chipType)
   }
   celsius = (float)raw / 16.0;
   return celsius;
-  
-}
-
-byte initOneWireTemperatureProbe(int powerPin, byte* addr, OneWire* ow)
-{
-  byte type_s = 0;
-  pinMode(powerPin, OUTPUT);
-  digitalWrite(powerPin, HIGH);    
-  if ( !ow->search(addr)) 
-  {
-    ow->reset_search();
-    delay(250);
-    return 0;
-  }
-   
-  // the first ROM byte indicates which chip
-  switch (addr[0]) 
-  {
-    case 0x10:
-      type_s = 1;
-      break;
-    case 0x28:
-      type_s = 0;
-      break;
-    case 0x22:
-      type_s = 0;
-      break;
-    default:
-      return 0;
-  } 
-  return type_s;
-}
-const int microLEDPin = 13;
-const int commLEDPin = 2;
-boolean commLED = true;
-
-struct TXinfo
-{
-  int cubeInit = 1;
-  int newSettingDone = 0;
-};
-struct RXinfo
-{
-  int newSetting = 0;
-};
-
-struct TX
-{
-  TXinfo txInfo;
-  TransmitData txData;
-};
-struct RX
-{
-  RXinfo rxInfo;
-  ReceiveData rxData;
-};
-TX tx;
-RX rx;
-ReceiveData settingsStorage;
-
-int sizeOfTx = 0;
-int sizeOfRx = 0;
-
-void setup()
-{
-  setupPins();
-  pinMode(microLEDPin, OUTPUT);    
-  pinMode(commLEDPin, OUTPUT);  
-  digitalWrite(commLEDPin, commLED);
-//  digitalWrite(microLEDPin, commLED);
-
-  sizeOfTx = sizeof(tx);
-  sizeOfRx = sizeof(rx);
-  Serial1.begin(BAUD_RATE);
-  delay(1000);
-}
-void loop()
-{
-  boolean goodData = false;
-  goodData = processData(&(tx.txData), &settingsStorage);
-  if (goodData)
-  {
-    tx.txInfo.newSettingDone = 0;
-    if(Serial1.available() > 0)
-    { 
-      commLED = !commLED;
-      digitalWrite(commLEDPin, commLED);
-      Serial1.readBytes((uint8_t*)&rx, sizeOfRx);
-      
-      if (rx.rxInfo.newSetting > 0)
-      {
-        processNewSetting(&(tx.txData), &settingsStorage, &(rx.rxData));
-        tx.txInfo.newSettingDone = 1;
-        tx.txInfo.cubeInit = 0;
-      }
-    }
-    Serial1.write((uint8_t*)&tx, sizeOfTx);
-  }
   
 }
